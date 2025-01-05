e13b13ae99e2d8a7728ee09cf65cf4716b5fb0ad
Kundan547 <kundanvyas197@gmail.com>
2025-01-05 12:53:18 +0530

    Added a python script to get the random joke from joke id.

---
+++ joke_id.py
@@ -0,0 +1,14 @@
+import requests
+
+url = "https://api.freeapi.app/api/v1/public/randomjokes/100"
+
+##querystring = {"limit":"10","query":"science","inc":"categories%2Cid%2Ccontent","page":"1"}
+
+headers = {"accpet": "application/json"}
+
+response = requests.get(url, headers=headers)
+
+if response.status_code == 200:
+    print(response.json())
+else:
+    print(f"Error: {response.status_code}, {response.text}")
