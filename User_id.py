8d5680e0836019fb689ad9f942244fbb8a42082e
Kundan547 <kundanvyas197@gmail.com>
2025-01-05 11:57:22 +0530

    Added a script to get userid by using api.

---
+++ User_id.py
@@ -0,0 +1,11 @@
+import requests
+
+url = "https://api.freeapi.app/api/v1/public/randomusers"
+headers = {"accept": "application/json"}
+querystring = {"limit":"1"}
+response = requests.get(url, headers=headers, params=querystring)
+
+if response.status_code == 200:
+    print(response.json())
+else:
+    print(f"Error: {response.status_code}, {response.text}")
