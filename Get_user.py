734bf35dc8bd5b29cd170dce50d44baa674ac296
Kundan547 <kundanvyas197@gmail.com>
2025-01-05 11:50:23 +0530

    Added a get user script to get user data from api and represent in json formet.

---
+++ Get_user.py
@@ -0,0 +1,14 @@
+import requests
+
+url = "https://api.freeapi.app/api/v1/public/randomusers"
+
+querystring = {"page":"1", "limit":"5"}
+headers = {"accept": "application/json"}
+
+response = requests.get(url, headers=headers, params = querystring)
+
+##print(response.json())
+if response.status_code == 200:
+    print(response.json())
+else:
+    print(f"Error: {response.status_code}, {response.text}")
