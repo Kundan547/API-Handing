8dedfc0743e0173b08c9def3f5195bda5ddbb48f
Kundan547 <kundanvyas197@gmail.com>
2025-02-03 23:19:59 +0530

    MOdify the api for better understanding.

--- Testing/Get_user.py
+++ Testing/Get_user.py
@@ -10,5 +10,6 @@ response = requests.get(url, headers=headers, params = querystring)
 ##print(response.json())
 if response.status_code == 200:
     print(response.json())
+    
 else:
     print(f"Error: {response.status_code}, {response.text}")
