3b2c7eaa71b74e2d75a732a49f4d65d6996cda3d
Kundan547 <kundanvyas197@gmail.com>
2025-02-03 23:20:32 +0530

    Added some chnages on api.

--- Testing/joke_id.py
+++ Testing/joke_id.py
@@ -10,5 +10,6 @@ response = requests.get(url, headers=headers)
 
 if response.status_code == 200:
     print(response.json())
+    
 else:
     print(f"Error: {response.status_code}, {response.text}")
