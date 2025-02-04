5089447162d2b751a7d8e11814c1e8d8e94b3fc0
Kundan547 <kundanvyas197@gmail.com>
2025-02-04 18:48:52 +0530

    Changes perform.

--- Testing/Get_user.py
+++ Testing/Get_user.py
@@ -10,6 +10,5 @@ response = requests.get(url, headers=headers, params = querystring)
 ##print(response.json())
 if response.status_code == 200:
     print(response.json())
-    
 else:
     print(f"Error: {response.status_code}, {response.text}")
